24c4266a4120f5d1115beeede11c2681031da706
Simon.Pan <simon.pan@eitc.com.tw>
2019-09-02 18:54:00 +0800

    將點擊的位置由xy直接指派改為搜圖尋位

--- scripts/LeCun_Login/index.js
+++ scripts/LeCun_Login/index.js
@@ -22,19 +22,37 @@ var appPackageName = 'com.h157945806.qsa';
 var rbm;
 
 var Click = function(x, y) {
-  rbm.click({x: x, y: y});
+  rbm.click({
+    x: x,
+    y: y
+  });
 };
 var TapDown = function(x, y) {
-  rbm.tapDown({x: x, y: y});
+  rbm.tapDown({
+    x: x,
+    y: y
+  });
 };
 var MoveTo = function(x, y) {
-  rbm.moveTo({x: x, y: y});
+  rbm.moveTo({
+    x: x,
+    y: y
+  });
 };
 var TapUp = function(x, y) {
-  rbm.tapUp({x: x, y: y});
+  rbm.tapUp({
+    x: x,
+    y: y
+  });
 };
 var Swipe = function(x1, y1, x2, y2) {
-  rbm.swipe({x: x1, y: y1}, {x: x2, y: y2}, 4);
+  rbm.swipe({
+    x: x1,
+    y: y1
+  }, {
+    x: x2,
+    y: y2
+  }, 4);
 };
 var Home = function() {
   keycode('HOME', 100);
@@ -59,26 +77,60 @@ var PrintColor = function(x, y) {
   rbm.log("Color R: " + c.r + " G: " + c.g + " B: " + c.b);
 }
 
+function findImagePosition(x1, y1, x2, y2, imageThreshold, imageName){
+
+  var result;
+  if (rbm !== undefined) {
+    rbm.log('findImagePosition('+imageName+')');
+
+    //抓圖辨識數字及x位置
+    rbm.keepScreenshotPartial(x1, y1, x2, y2);
+
+
+    var results = rbm.findImages(imageName, imageThreshold, 1, false, false);
+
+    var arrynum = 1
+
+		for (var index in results){
+			result = results[index];
+		// 	rbm.log('result.x'+result.x+', result.y: '+result.y);
+			arrynum = arrynum + 1
+		}
+    rbm.releaseScreenshot();
+  }
+    return result;
+}
+
+function tapImage(x1,y1,x2,y2, imageName, imageThreshold){
+  if (rbm !== undefined) {
+    rbm.log('tapImage('+imageName+')');
+
+  	rbm.keepScreenshotPartial(x1, y1, x2, y2);
+	  rbm.imageClick(imageName, imageThreshold);
+	  rbm.releaseScreenshot();
+  }
+}
+
 function safeSleep(t) {
   if (t === undefined) {
     t = 200;
   }
-  
+
   var start = Date.now();
   if (rbm !== undefined) {
-      while(rbm.running) {
-        sleep(200);
-        if (Date.now() - start >= t) {
-          break;
-        }
-     }
+    while (rbm.running) {
+      sleep(200);
+      if (Date.now() - start >= t) {
+        break;
+      }
+    }
   }
 }
 
 function start(account, password) {
   stop();
   var screenSize = getScreenSize();
-    
+
   var config = {
 
     oriScreenWidth: screenSize.width,
@@ -99,23 +151,29 @@ function start(account, password) {
   rbm.startApp(appPackageName, 'com.uzmap.pkg.EntranceActivity');
 
   safeSleep(5000);
-  
+
   StartLogin(account, password);
 }
 
 function StartLogin(account, password) {
-  rbm.log('[LeCun] StartLogin('+account+'::'+password+')');
+  rbm.log('[LeCun] StartLogin(' + account + '::' + password + ')');
 
-  //登入帳號
-  tap(284, 810, 10);
+  // 登入帳號
+  tapImage(176, 742, 637, 861, 'field_account.png', 0.7);
   safeSleep(1000);
   rbm.typing(account)
 
   safeSleep(500);
 
+  //返回鍵
+  Back();
+
+  safeSleep(500);
+
   //登入密碼
-  tap(306, 998, 10);
-  rbm.typing(password, 100);
+  tapImage(180, 937, 867, 1081, 'field_pass.png', 0.7);
+  safeSleep(1000);
+  rbm.typing(password);
 
   //返回鍵
   Back();
@@ -123,15 +181,17 @@ function StartLogin(account, password) {
   safeSleep(1000);
 
   //按下登入鈕
-  tap(525, 1232, 100);
+  tapImage(61, 1063, 993, 1373, 'btn_login.png', 0.7);
 
   safeSleep(1000);
 
-  tap(298, 955, 100);
+  //按下請輸入圖形驗證碼
+  tapImage(75, 746, 975, 1063, 'field_verify.png', 0.7);
 
   safeSleep(1000);
 
-  tap(74, 1945, 100);
+  //按下鍵盤上數字切換
+  tapImage(0, 1800, 200, 2075, 'field_keyboard_num.png', 0.7);
 }
 
 function stop() {
@@ -141,3 +201,5 @@ function stop() {
     rbm.log('[LeCun] Stop');
   }
 }
+
+// start('account', 'pass');
